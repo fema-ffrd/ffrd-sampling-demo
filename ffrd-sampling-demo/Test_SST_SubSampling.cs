a2b4d540154dfd250b850371a4bcde7be5161a0d
HadenSmith <cole.h.smith@usace.army.mil>
2025-07-01 15:40:29 -0600

    I improved the sub-sampling routine to reject k-NN samples that do not fall within the correct strata. This results in much improved performance in the extreme tails.

--- ffrd-sampling-demo/Test_SST_SubSampling.cs
+++ ffrd-sampling-demo/Test_SST_SubSampling.cs
@@ -46,7 +46,7 @@ namespace Test_FFRD
 
             // D = number of target sites; N = number of Monte Carlo events
             int D = 5;
-            int N = 100000;
+            int N = 10000;
 
             // Average inter-site correlation 
             // This method works best when correlations are relatively large > 0.5. 
@@ -176,6 +176,175 @@ namespace Test_FFRD
                 var x = CreateXValues(minMax[0, d], minMax[1, d], 100, true);
                 // 9b) Compute AEP curve via Total Probability
                 var aepX = CreateFrequencyCurve(x, weights, results[d]);
+                // 9c) Interpolate to target AEPs
+                curves.Add(InterpolateFrequencyCurve(aeps, aepX, true));
+
+                // Print results for debugging
+                for (int i = 0; i < aeps.Length; i++)
+                {
+                    Debug.WriteLine(aeps[i].ToString() + "," + curves.Last()[i, 1].ToString());
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// I have improved this by rejecting k-NN samples that do not fall within the desired strata.
+        /// </summary>
+        [TestMethod]
+        public void Test_kNN_SubSampling_ToyProblem_V2()
+        {
+            // Marginal distributions that represent target flow sites
+            var dists = new Normal[] { new Normal(10, 2), new Normal(30, 15), new Normal(17, 5), new Normal(99, 14), new Normal(68, 7) };
+
+            // D = number of target sites; N = number of Monte Carlo events
+            int D = 5;
+            int N = 100000;
+
+            // Average inter-site correlation 
+            // This method works best when correlations are relatively large > 0.5. 
+            // You can change rho and compare results. 
+            double rho = 0.5;
+            var mean = new double[] { 0, 0, 0, 0, 0 };
+            var covar = new double[,]
+                            {{ 1, rho, rho, rho, rho },
+                             { rho, 1, rho, rho, rho },
+                             { rho, rho, 1, rho, rho },
+                             { rho, rho, rho, 1, rho },
+                             { rho, rho, rho, rho, 1 }};
+            var mvn = new MultivariateNormal(mean, covar);
+            var rnd = mvn.GenerateRandomValues(N, 12345);
+
+            // Prepare arrays for k-NN features
+            var yVals = new double[N];      // Dummy response vector (sum of flows)
+            var xVals = new double[N, D];   // Raw flows at each site
+            var xPP = new double[N, D + 1]; // Weibull plotting positions + joint prob
+            var xZ = new double[N, D + 1];  // Standard-normal transformed positions
+
+            // 2) Populate xVals and yVals
+            for (int i = 0; i < N; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < D; j++)
+                {
+                    // Sample flow at site j
+                    xVals[i, j] = dists[j].InverseCDF(Normal.StandardCDF(rnd[i, j]));
+                    sum += xVals[i, j];
+                }
+                yVals[i] = sum; // Use sum as dummy y for k-NN
+            }
+
+            // 3) Convert each site marginal to plotting positions (Weibull) and standard-normal
+            for (int j = 0; j < D; j++)
+            {
+                // Rank data in-place: lowest to highest
+                var ranks = Statistics.RanksInPlace(xVals.GetColumn(j));
+                // Weibull plotting position: rank / (N+1)
+                xPP.SetColumn(j, ranks.Divide(N + 1));
+                // Z-transform of plotting positions
+                xZ.SetColumn(j, Normal.StandardZ(ranks.Divide(N + 1)));
+            }
+
+            // 4) Estimate covariance of site Zs to build Gaussian copula
+            var covMatrix = new RunningCovarianceMatrix(D);
+            for (int i = 0; i < N; i++)
+            {
+                // Push each D-dimensional Z-vector (exclude last joint column)
+                covMatrix.Push(xZ.GetRow(i).Subset(0, D - 1));
+            }
+            // Create multivariate normal with estimated mean and scaled covariance
+            var mvnZ = new MultivariateNormal(covMatrix.Mean.GetColumn(0), (1d / N * covMatrix.Covariance).ToArray());
+            // Create inputs for the product of conditional marginals (PCM) method
+            var corr = mvnZ.Covariance;
+            var ind = new int[D];
+            ind.Fill(1);
+
+            // 5) Compute joint plotting positions via high-dimensional copula CDF
+            for (int i = 0; i < N; i++)
+            {
+                // Joint probability under Gaussian copula
+                xPP[i, D] = Probability.JointProbabilityHPCM(xPP.GetRow(i).Subset(0, D - 1), ind, corr);
+                // Z-transform of joint probability
+                xZ[i, D] = Normal.StandardZ(xPP[i, D]);
+            }
+
+            // 6) Build k-NN sampler on (D+1)-dim Z-space, using yVals as dummy
+            int K = 100;
+            var kNN = new KNearestNeighbors(xZ, yVals, K) { IsRegression = false }; // we only need indices, not predictions
+
+            // 7) Stratify Z-space into bins for the joint Z dimension
+            int bins = 10;
+            var stratOpts = new StratificationOptions(0.05, 0.9999, bins, true);
+            var strata = Stratify.Probabilities(stratOpts, Stratify.ImportanceDistribution.Normal, true);
+            var weights = strata.Select(w => w.Weight).ToArray();
+
+            // Prepare storage for sub-sampled flows per site & per bin
+            var minMax = new double[2, D];
+            var results = new List<double[,]>();
+            var kValues = new int[bins, D];
+            for (int i = 0; i < D; i++)
+            {
+                results.Add(new double[bins, K]);
+                minMax[0, i] = double.MaxValue;
+                minMax[1, i] = double.MinValue;
+            }
+
+            // 8) Sub-sample via k-NN within each bin centroid in Z-space
+            for (int i = 0; i < bins; i++)
+            {
+                // Build feature vector for bin "midpoint" in Z-space
+                var zQuery = new double[1, D + 1];
+                for (int j = 0; j < D; j++)
+                {
+                    // Marginal Z = StandardZ(bin midpoint)
+                    zQuery[0, j] = Normal.StandardZ(strata[i].Midpoint);
+                }
+                // Joint Z: feed marginals through multivariate CDF then transform
+                var jp = Normal.StandardCDF(zQuery.GetRow(0).Subset(0, D - 1));
+                zQuery[0, D] = Normal.StandardZ(Probability.JointProbabilityHPCM(jp, ind, corr));
+
+                // Get the K nearest neighbors in Z-space
+                var neighborIndices = kNN.GetNeighbors(zQuery);
+
+                // In this update, we only include the values that fall within the correct strata
+                double zLower = Normal.StandardZ(strata[i].LowerBound);
+                double zUpper = Normal.StandardZ(strata[i].UpperBound);
+
+                // Extract original flows for each site & update min/max
+                for (int d = 0; d < D; d++)
+                {
+                    for (int k = 0; k < K; k++)
+                    {
+                        // Check if the sampled value falls within the correct strata
+                        double z = xZ[neighborIndices[k], d];
+                        if (z >= zLower && z < zUpper)
+                        {
+                            kValues[i, d]++;
+                            double val = xVals[neighborIndices[k], d];
+                            results[d][i, k] = val;
+                            minMax[0, d] = Math.Min(minMax[0, d], val);
+                            minMax[1, d] = Math.Max(minMax[1, d], val);
+                        }
+                        else
+                        {
+                            results[d][i, k] = double.NaN;
+                        }
+                    }
+                }
+
+            }
+
+            // 9) Post-process each site: reconstruct frequency curves
+            var aeps = new double[] { 0.9, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001, 0.0005 };
+            var curves = new List<double[,]>();
+
+            for (int d = 0; d < D; d++)
+            {
+                // 9a) Construct an x-grid between observed min & max
+                var x = CreateXValues(minMax[0, d], minMax[1, d], 100, true);
+                // 9b) Compute AEP curve via Total Probability
+                var aepX = CreateFrequencyCurve(x, weights, kValues.GetColumn(d), results[d]);
                 // 9c) Interpolate to target AEPs
                 curves.Add(InterpolateFrequencyCurve(aeps, aepX, true));
 
@@ -351,6 +520,162 @@ namespace Test_FFRD
 
         }
 
+        /// <summary>
+        /// I have improved this by rejecting k-NN samples that do not fall within the desired strata.
+        /// </summary>
+        [TestMethod]
+        public void Test_kNN_SubSampling_Kanawha_14_sites_V2()
+        {
+            // 1) Load raw CSV into jagged array: columns: [ID, ..., site flows...]
+            // Change file location as needed
+            var data = ReadCsvFile("C:/Users/Q0RMCCHS/Documents/0_RMC_Work/FFRD/Kanawha Basin Results/simulation_peaks_by_event_primary.csv");
+
+            // D = number of target sites; N = number of Monte Carlo events
+            int D = 14;
+            int N = data.Length;
+
+            // Prepare arrays for k-NN features
+            var yVals = new double[N];      // Dummy response vector (sum of flows)
+            var xVals = new double[N, D];   // Raw flows at each site
+            var xPP = new double[N, D + 1]; // Weibull plotting positions + joint prob
+            var xZ = new double[N, D + 1];  // Standard-normal transformed positions
+
+            // 2) Populate xVals and yVals
+            for (int i = 0; i < N; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < D; j++)
+                {
+                    // Pick off flow at site j (assuming columns 3..)
+                    xVals[i, j] = data[i][j + 3];
+                    sum += xVals[i, j];
+                }
+                yVals[i] = sum;               // Use sum as dummy y for k-NN
+            }
+
+            // 3) Convert each site marginal to plotting positions (Weibull) and standard-normal
+            for (int j = 0; j < D; j++)
+            {
+                // Rank data in-place: lowest to highest
+                var ranks = Statistics.RanksInPlace(xVals.GetColumn(j));
+                // Weibull plotting position: rank / (N+1)
+                xPP.SetColumn(j, ranks.Divide(N + 1));
+                // Z-transform of plotting positions
+                xZ.SetColumn(j, Normal.StandardZ(ranks.Divide(N + 1)));
+            }
+
+            // 4) Estimate covariance of site Zs to build Gaussian copula
+            var covMatrix = new RunningCovarianceMatrix(D);
+            for (int i = 0; i < N; i++)
+            {
+                // Push each D-dimensional Z-vector (exclude last joint column)
+                covMatrix.Push(xZ.GetRow(i).Subset(0, D - 1));
+            }
+            // Create multivariate normal with estimated mean and scaled covariance
+            var mvnZ = new MultivariateNormal(covMatrix.Mean.GetColumn(0), (1d / N * covMatrix.Covariance).ToArray());
+            // Create inputs for the product of conditional marginals (PCM) method
+            var corr = mvnZ.Covariance;
+            var ind = new int[D];
+            ind.Fill(1);
+
+            // 5) Compute joint plotting positions via high-dimensional copula CDF
+            for (int i = 0; i < N; i++)
+            {
+                // Joint probability under Gaussian copula
+                xPP[i, D] = Probability.JointProbabilityHPCM(xPP.GetRow(i).Subset(0, D - 1), ind, corr);
+                // Z-transform of joint probability
+                xZ[i, D] = Normal.StandardZ(xPP[i, D]);
+            }
+
+            // 6) Build k-NN sampler on (D+1)-dim Z-space, using yVals as dummy
+            int K = 100;
+            var kNN = new KNearestNeighbors(xZ, yVals, K) { IsRegression = false }; // we only need indices, not predictions
+
+            // 7) Stratify Z-space into bins for the joint Z dimension
+            int bins = 10;
+            var stratOpts = new StratificationOptions(0.05, 0.9999, bins, true);
+            var strata = Stratify.Probabilities(stratOpts, Stratify.ImportanceDistribution.Normal, true);
+            var weights = strata.Select(w => w.Weight).ToArray();
+
+            // Prepare storage for sub-sampled flows per site & per bin
+            var minMax = new double[2, D];
+            var results = new List<double[,]>();
+            var kValues = new int[bins, D];
+            for (int i = 0; i < D; i++)
+            {
+                results.Add(new double[bins, K]);
+                minMax[0, i] = double.MaxValue;
+                minMax[1, i] = double.MinValue;
+            }
+
+            // 8) Sub-sample via k-NN within each bin centroid in Z-space
+            for (int i = 0; i < bins; i++)
+            {
+                // Build feature vector for bin "midpoint" in Z-space
+                var zQuery = new double[1, D + 1];
+                for (int j = 0; j < D; j++)
+                {
+                    // Marginal Z = StandardZ(bin midpoint)
+                    zQuery[0, j] = Normal.StandardZ(strata[i].Midpoint);
+                }
+                // Joint Z: feed marginals through multivariate CDF then transform
+                var jp = Normal.StandardCDF(zQuery.GetRow(0).Subset(0, D - 1));
+                zQuery[0, D] = Normal.StandardZ(Probability.JointProbabilityHPCM(jp, ind, corr));
+
+                // Get the K nearest neighbors in Z-space
+                var neighborIndices = kNN.GetNeighbors(zQuery);
+
+                // In this update, we only include the values that fall within the correct strata
+                double zLower = Normal.StandardZ(strata[i].LowerBound);
+                double zUpper = Normal.StandardZ(strata[i].UpperBound);
+
+                // Extract original flows for each site & update min/max
+                for (int d = 0; d < D; d++)
+                {
+                    for (int k = 0; k < K; k++)
+                    {
+                        // Check if the sampled value falls within the correct strata
+                        double z = xZ[neighborIndices[k], d];
+                        if (z >= zLower && z < zUpper)
+                        {
+                            kValues[i, d]++;
+                            double val = xVals[neighborIndices[k], d];
+                            results[d][i, k] = val;
+                            minMax[0, d] = Math.Min(minMax[0, d], val);
+                            minMax[1, d] = Math.Max(minMax[1, d], val);
+                        }
+                        else
+                        {
+                            results[d][i, k] = double.NaN;
+                        }
+                    }
+                }
+
+            }
+
+            // 9) Post-process each site: reconstruct frequency curves
+            var aeps = new double[] { 0.9, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.002, 0.001, 0.0005 };
+            var curves = new List<double[,]>();
+
+            for (int d = 0; d < D; d++)
+            {
+                // 9a) Construct an x-grid between observed min & max
+                var x = CreateXValues(minMax[0, d], minMax[1, d], 100, true);
+                // 9b) Compute AEP curve via Total Probability
+                var aepX = CreateFrequencyCurve(x, weights, kValues.GetColumn(d), results[d]);
+                // 9c) Interpolate to target AEPs
+                curves.Add(InterpolateFrequencyCurve(aeps, aepX, true));
+
+                // Print results for debugging
+                for (int i = 0; i < aeps.Length; i++)
+                {
+                    Debug.WriteLine(aeps[i].ToString() + "," + curves.Last()[i, 1].ToString());
+                }
+
+            }
+
+        }
+
         #region Helpers
 
         /// <summary>
@@ -430,6 +755,37 @@ namespace Test_FFRD
                     // Accumulate weighted exceedance probability
                     // Update AEP using Total Probability
                     curve[i, 0] += weights[j] * exceed / M;
+                }
+                curve[i, 1] = xValues[i];
+            }
+
+            return curve;
+        }
+
+        public double[,] CreateFrequencyCurve(double[] xValues, double[] weights, int[] kValues, double[,] samples)
+        {
+
+            int D = xValues.Length;
+            int N = samples.GetLength(0);   // number of strata
+            int M = samples.GetLength(1);   // samples per stratum
+            var curve = new double[D, 2];   // [AEP, x]
+
+            for (int i = 0; i < D; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    // Count fraction of samples in stratum j exceeding xValues[i]
+                    int exceed = 0;
+                    for (int k = 0; k < M; k++)
+                    {
+                        if (!double.IsNaN(samples[j, k]) && samples[j, k] > xValues[i])
+                        {
+                            exceed++;
+                        }
+                    }
+                    // Accumulate weighted exceedance probability
+                    // Update AEP using Total Probability
+                    curve[i, 0] += weights[j] * exceed / kValues[j];
                 }
                 curve[i, 1] = xValues[i];
             }
